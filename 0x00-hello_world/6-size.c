2d4ba67583e1d7cc705cb9f16bc799b49786776c
SharonObange <obangesharon@gmail.com>
2023-06-15 16:38:00 -0700

    Print the size of Data types in 64 bit computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,12 @@
 /*Sizeof function*/
 #include <stdio.h>
 int main(void)
-/* main entry point*/
+/*main entry point*/
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of a int: %lu byte(s)\n", sizeof(int));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
