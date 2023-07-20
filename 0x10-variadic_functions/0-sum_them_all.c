507f4e3135255ad65034a1aa5f036b161511bca2
SharonObange <obangesharon@gmail.com>
2023-07-20 11:19:17 -0700

    Sum of arguments in variadic function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,26 +5,29 @@
  *sum_them_all - sums up all arguments
  *@n: number of arguments
  *Return: sum of arguments, 0 if n is 0
+ *@...: variable number of parameters
 */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list args;
-	unsigned int sum, x;
-
-	if (n == 0)
+	va_list arg;
+	unsigned int sum, x = 0;
+	
+	va_start(arg, n);
+	
+	for (x = 0; x < n; x++)
 	{
-		return (0);
-	}	
-
-	va_start(args, n);
-
-	for (x = 0; x <= n; x++)
-	{
-		sum += va_arg(args, int);
+		if (n == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			sum += va_arg(arg, int);
+		}
 	}
 
-	va_end(args);
+	va_end(arg);
 
 	return (sum);
 }
