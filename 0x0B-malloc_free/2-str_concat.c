4e1cad80029c10c41a9168c0f2fc5235bfa10eec
SharonObange <obangesharon@gmail.com>
2023-07-30 11:56:24 -0700

    Memory space allocation correction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,21 +17,18 @@ char *str_concat(char *s1, char *s2)
 	int y = 0;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
+	while (s1[x] != '\0')
+		x++;
 	if (s2 == NULL)
-	{
-
 		s2 = "";
-	}
-
+	while (s2[y] != '\0')
+		y++;
 	concat_str = malloc(sizeof(char) * (x + y + 1));
 
 	if (concat_str == NULL)
-	{
 		return (NULL);
-	}
+	x = y = 0;
 
 	while (s1[x] != '\0')
 	{
