ff21b048c7eaffb51cb0bed6b37bd0dbabcfc728
SharonObange <obangesharon@gmail.com>
2023-08-01 12:00:55 -0700

    fix Betty errorwq

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int x;
 	listint_t *prevNode = NULL;
 	listint_t *currentNode = *head;
-	
+
 	if (head == NULL || *head == NULL)
 	{
 		return (-1);
